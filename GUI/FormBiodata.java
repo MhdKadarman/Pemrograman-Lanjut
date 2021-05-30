f59ada8a22b82addd12c81d2f013cb089a858e16
Mhd. Kadarman <80524634+MhdKadarman@users.noreply.github.com>
2021-05-30 14:38:13 +0700

    Add files via upload

---
+++ GUI/FormBiodata.java
@@ -0,0 +1,36 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FormBiodata {
+    private JTextField textNama;
+    private JTextField textNim;
+    private JButton buttonSave;
+    private JPanel rootPanel;
+    private JLabel labelHasil;
+
+    public FormBiodata() {
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Mengambil data dari textNama
+                String nama = textNama.getText();
+                //Mengambil data dari textNim
+                String nim = textNim.getText();
+                //Diproses
+                Mahasiswa mhs = new Mahasiswa();
+                mhs.setNama(nama);
+                mhs.setNim(nim);
+                //Tampilkan output ke FORM
+                labelHasil.setText(mhs.toString());
+
+            }
+        });
+    }
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+}
